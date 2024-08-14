9e5bd0ad4c3b292383a7167d9c51f95bac887eef
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-14 14:45:23 +0600

    refactor get, post requests;

--- guestbook-api/routers/messages.ts
+++ guestbook-api/routers/messages.ts
@@ -1,13 +1,28 @@
 import express from "express";
+import fileDb from "../fileDb";
+import {MessageType} from "../types";
 
 const messagesRouter = express.Router();
 
-messagesRouter.get("/", (req, res) => {
-    return res.send("Here are your messages: ");
+messagesRouter.get("/", async (req, res) => {
+    const allMessages = await fileDb.getMessages();
+
+    return res.send(allMessages);
 });
 
-messagesRouter.post("/", (req, res) => {
-    return res.send(req.body);
+messagesRouter.post("/", async (req, res) => {
+    if (!req.body.message) {
+        return res.status(400).send({error: "Message must be present in the request"});
+    }
+    const message: MessageType = {
+        author: req.body.author,
+        message: req.body.message,
+        image: req.body.image,
+    };
+
+    const savedMessage = await fileDb.addMessage(message);
+
+    return res.send(savedMessage);
 });
 
 export default messagesRouter;
