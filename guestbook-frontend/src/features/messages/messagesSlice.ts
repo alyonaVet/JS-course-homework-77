4204c0c1845cb100b1d21bb12f38745144a3745f
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-16 13:05:59 +0600

    added builder for create message, added selectMessageCreating;

--- guestbook-frontend/src/features/messages/messagesSlice.ts
+++ guestbook-frontend/src/features/messages/messagesSlice.ts
@@ -1,16 +1,17 @@
 import {createSlice} from '@reduxjs/toolkit';
 import {Message} from '../../types';
-import {fetchMessages} from './messagesThunks';
+import {createMessage, fetchMessages} from './messagesThunks';
 
 export interface MessagesState {
   messages: Message[];
   messagesFetching: boolean;
-
+  isCreating: boolean;
 }
 
 const initialState: MessagesState = {
   messages: [],
   messagesFetching: false,
+  isCreating: false,
 };
 
 export const messagesSlice = createSlice({
@@ -29,10 +30,21 @@ export const messagesSlice = createSlice({
       .addCase(fetchMessages.rejected, (state) => {
         state.messagesFetching = false;
       });
+    builder
+      .addCase(createMessage.pending, (state) => {
+        state.isCreating = true;
+      })
+      .addCase(createMessage.fulfilled, (state) => {
+        state.isCreating = false;
+      })
+      .addCase(createMessage.rejected, (state) => {
+        state.isCreating = false;
+      });
   },
   selectors: {
     selectMessages: (state) => state.messages,
     selectMessagesFetching: (state) => state.messagesFetching,
+    selectMessageCreating: (state) => state.isCreating,
 
   }
 });
@@ -42,4 +54,5 @@ export const messagesReducer = messagesSlice.reducer;
 export const {
   selectMessages,
   selectMessagesFetching,
+  selectMessageCreating
 } = messagesSlice.selectors;
