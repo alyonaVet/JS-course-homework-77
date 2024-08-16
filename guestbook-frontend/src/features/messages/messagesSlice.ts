3e99f7079b1e3d9eae2a644399d6efd3ce575063
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-16 12:03:51 +0600

    added extraReducers, selectors;

--- guestbook-frontend/src/features/messages/messagesSlice.ts
+++ guestbook-frontend/src/features/messages/messagesSlice.ts
@@ -1,19 +1,45 @@
 import {createSlice} from '@reduxjs/toolkit';
 import {Message} from '../../types';
+import {fetchMessages} from './messagesThunks';
 
 export interface MessagesState {
   messages: Message[];
+  messagesFetching: boolean;
+
 }
 
 const initialState: MessagesState = {
   messages: [],
-
+  messagesFetching: false,
 };
 
 export const messagesSlice = createSlice({
   name: "messages",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMessages.pending, (state) => {
+        state.messagesFetching = true;
+      })
+      .addCase(fetchMessages.fulfilled, (state, {payload: messages}) => {
+        state.messagesFetching = false;
+        state.messages = messages;
+      })
+      .addCase(fetchMessages.rejected, (state) => {
+        state.messagesFetching = false;
+      });
+  },
+  selectors: {
+    selectMessages: (state) => state.messages,
+    selectMessagesFetching: (state) => state.messagesFetching,
+
+  }
 });
 
 export const messagesReducer = messagesSlice.reducer;
+
+export const {
+  selectMessages,
+  selectMessagesFetching,
+} = messagesSlice.selectors;
