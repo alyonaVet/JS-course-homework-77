33b7047c22b5402df5eb943e78d16ed13ac400db
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-16 17:07:02 +0600

    added Spinner when load messages;

--- guestbook-frontend/src/features/messages/Messages.tsx
+++ guestbook-frontend/src/features/messages/Messages.tsx
@@ -1,13 +1,15 @@
 import {useAppDispatch, useAppSelector} from '../../app/hooks';
-import {selectMessages} from './messagesSlice';
+import {selectMessages, selectMessagesFetching} from './messagesSlice';
 import {Box, Grid} from '@mui/material';
 import OneMessage from './components/OneMessage';
 import {useEffect} from 'react';
 import {fetchMessages} from './messagesThunks';
+import Spinner from '../../UI/Spinner/Spinner';
 
 const Messages = () => {
   const messages = useAppSelector(selectMessages);
   const dispatch = useAppDispatch();
+  const messagesLoading = useAppSelector(selectMessagesFetching);
 
   useEffect(() => {
     dispatch(fetchMessages());
@@ -16,15 +18,18 @@ const Messages = () => {
   return (
     <Box sx={{ p: 2 }}>
       <Grid container spacing={3}>
-        {messages.map((message) => (
-          <Grid item xs={12} sm={6} md={4} lg={3} key={message.id}>
-            <OneMessage
-              author={message.author}
-              message={message.message}
-              image={message.image}
-            />
-          </Grid>
-        ))}
+        {messagesLoading ? (
+          <Spinner />
+        ) : (
+          messages.map((message) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={message.id}>
+              <OneMessage
+                author={message.author}
+                message={message.message}
+                image={message.image}
+              />
+            </Grid>
+          )))}
       </Grid>
     </Box>
   );
